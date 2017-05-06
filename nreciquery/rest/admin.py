a3fdd7a79799384a6901d3525541d25aa45dbf3b
yev <josephharveyangeles@gmail.com>
2017-05-06 22:46:44 +0800

    Implemented initial db models

--- nreciquery/rest/admin.py
+++ nreciquery/rest/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Ingredient, Recipe, Condiment
 # Register your models here.
+
+admin.site.register(Ingredient)
+admin.site.register(Condiment)
+admin.site.register(Recipe)
--- nreciquery/rest/models.py
+++ nreciquery/rest/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Condiment(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField(default="")
+
+    STARTER = 'ST'
+    MAIN_DISH = 'MD'
+    SIDE_DISH = 'SD'
+    SNACK = 'SN'
+    DESSERT = 'DS'
+    TYPES = (
+        (STARTER, 'Started'),
+        (MAIN_DISH, 'Main Dish'),
+        (SIDE_DISH, 'Side Dish'),
+        (SNACK, 'Snack'),
+        (DESSERT, 'Dessert'),
+    )
+    type = models.CharField(max_length=2, choices=TYPES, default=MAIN_DISH)
+
+    ingredients = models.ManyToManyField(Ingredient)
+    condiments = models.ManyToManyField(Condiment)
+    ingredients_details = models.TextField(default="")
+    directions = models.TextField(default="")
+
+    def __str__(self):
+        return self.name
